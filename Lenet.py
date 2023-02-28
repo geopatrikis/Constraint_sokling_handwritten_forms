ef941d620fe45ec855d4f0b6672edea93e99c48e
Giorgos Patrikis <geopatrikis12@gmail.com>
2023-02-28 18:50:59 +0100

    digit_ocr_nn.ipynb is used to create a model for digit extraction. After creating it we have to use it to predict labels in extract_character.py TODO: preprocces input image to be identified correctly.

---
+++ Lenet.py
@@ -0,0 +1,23 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class LeNet(nn.Module):
+    def __init__(self, calibrated=False):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, 1, padding=2)
+        self.conv2 = nn.Conv2d(6, 16, 5, 1)
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84,10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 5*5*16)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
--- extract_character.py
+++ extract_character.py
@@ -1,8 +1,15 @@
 import os
 import cv2
+import numpy as np
+import torch
+from PIL import Image
+from torchvision import transforms
+from matplotlib import pyplot as plt
+
+from Lenet import LeNet
 
 
-def extract_characters(img_name):
+def extract_characters(img_name,model):
     img = cv2.imread(img_name)
 
     # Convert the image to grayscale
@@ -31,16 +38,54 @@ def extract_characters(img_name):
                 break
         # Get the bounding rectangle of the contour
         if 30 < w < 100 and 40 < h < 100 and black_found:
-            cv2.rectangle(img, (x+3, y+3), (x + w-3, y + h-3), (0, 255, 0), 2)
+            show_one_prediction(character,model)
+            cv2.rectangle(img, (x + 3, y + 3), (x + w - 3, y + h - 3), (0, 255, 0), 2)
 
     # Display the image with bounding boxes
     cv2.imshow('image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+def show_one_prediction(img, model, i=0):
+    gray_roi = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resized_roi = cv2.resize(gray_roi, (28, 28))/255
+    # Turn off gradients to speed up this part
+    img_tensor = torch.from_numpy(resized_roi).unsqueeze(0).float()
+    # Define a transform to normalize the data
+    # Apply the transform to the tensor
+    img_tensor_normalized = (img_tensor - 0.5) / -0.5
+    print(img_tensor_normalized)
+    with torch.no_grad():
+        logps = model(img_tensor_normalized)
+
+    # Output of the network are log-probabilities, need to take exponential for probabilities
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    print(("pred:",probab.index(max(probab))))
+    cv2.imshow("actual",img)
+    cv2.waitKey(0)
+
+def predict_letter(img,model):
+    print(img.shape)
+    cv2.imshow('image', img)
+    gray_roi = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resized_roi = cv2.resize(gray_roi, (28, 28))
+    normalized_roi = resized_roi / 255.0
+    #pil_image = Image.fromarray((normalized_roi * 255).astype('uint8'))
+    logpros=model(normalized_roi)
+    cv2.waitKey(0)
+    # Apply thresholding to make the image binary
+    # Use Tesseract OCR to recognize text
+    # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    # Print the recognized text
+
+
 # extract file name without extension
 if __name__ == '__main__':
-    #extract_characters('C:/Users/30698/Desktop/KULeuven/Capita Selecta/formes/fields/0001_DATE.jpg')
+    model_digits = LeNet()
+    model_digits.load_state_dict(torch.load('saved_models/digits_model_nn'))
+    # extract_characters('C:/Users/30698/Desktop/KULeuven/Capita Selecta/formes/fields/0001_DATE.jpg')
     dir_path = 'C:/Users/30698/Desktop/KULeuven/Capita Selecta/formes/fields'
     # Define the allowed image extensions
     img_extensions = [".jpg", ".jpeg", ".png"]
@@ -55,4 +100,4 @@ if __name__ == '__main__':
             if any(file.endswith(ext) for ext in img_extensions):
                 # Add the full path of the image to the list
                 img_path = os.path.join(root, file)
-                extract_characters(img_path)
+                extract_characters(img_path,model_digits)
